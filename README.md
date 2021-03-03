d45224603a639f1c6b18439f303833145b46c415
saurav <stomatrix@gmail.com>
2021-03-03 22:47:08 +0530

    first blood

---
+++ README.md
@@ -0,0 +1,17 @@
+# mudrex-ropsten subgraph
+
+
+Based on the example subgraph [https://github.com/graphprotocol/example-subgraph](https://github.com/graphprotocol/example-subgraph).
+
+ For more information see the docs on [https://thegraph.com/docs/](https://thegraph.com/docs/).
+
+
+# Setup
+```
+yarn install
+pip install eth-brownie
+brownie compile
+
+yarn codegen
+yarn deploy
+```
---
+++ truffle.js
@@ -0,0 +1,27 @@
+require('babel-register')
+require('babel-polyfill')
+const HDWalletProvider = require('truffle-hdwallet-provider')
+
+module.exports = {
+  networks: {
+    development: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: '*',
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          `https://ropsten.infura.io/v3/741ef88b1e1d4a4d97268a379983c9f9`
+        )
+      },
+      network_id: '3',
+    },
+  },
+  compilers: {
+    solc: {
+      version: '0.4.25'    // Fetch exact version from solc-bin (default: truffle's version)
+    }
+  }
+}
